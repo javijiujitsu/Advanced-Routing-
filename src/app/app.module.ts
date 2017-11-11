80c294b1f41fdc1be34b8a80a6b59c3af54e6909
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-11 15:11:16 -0500

    edit is working somewhat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,26 @@ import { ContactServiceService } from './contact-service.service';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { ContactOverviewComponent } from './contact-overview/contact-overview.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 const routes: Routes = [
   { path: '', component: ContactListComponent },
-  { path: 'contact/:id', component: ContactComponent }
-
+  { path: 'contact/:id', component: ContactComponent,
+    children: [
+      { path: '', component: ContactOverviewComponent },
+      { path: 'edit', component: ContactEditComponent }
+    ]
+  }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    ContactComponent
+    ContactComponent,
+    ContactOverviewComponent,
+    ContactEditComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-edit/contact-edit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContactServiceService } from '../contact-service.service';
+
+@Component({
+  selector: 'app-contact-edit',
+  templateUrl: './contact-edit.component.html',
+  styleUrls: ['./contact-edit.component.css'],
+
+})
+export class ContactEditComponent implements OnInit {
+  originalContact: any;
+  editContact: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactServiceService
+  ) { }
+
+  ngOnInit() {
+    let paramId = +this.route.snapshot.parent.params['id'];
+    this.originalContact = this.contactService.get(paramId);
+    this.editContact = {
+      id: this.originalContact.id,
+      name: this.originalContact.name
+    };
+  }
+
+  save() {
+    this.originalContact = this.contactService.edit(this.editContact);
+  }
+}
